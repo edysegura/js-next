994e2f8aa0a44ac31299afb72d71d7cdbb0adfbb
Edy Segura <ednardo@inatel.br>
2018-04-09 23:06:40 -0300

    Update map example

--- data-structure/map.js
+++ data-structure/map.js
@@ -6,6 +6,11 @@ jsMap.set('key2', 'value2')
 jsMap.set('key3', 'value3')
 jsMap.set('key4', 'value4')
 
+const objectKey = { email:'infoedy@gmail.com' }
+jsMap.set(objectKey, 'value5')
+
 for (const [ key, value ] of jsMap) {
   console.log(key, value)
 }
+
+console.log(jsMap.get(objectKey))
