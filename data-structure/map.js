6a33179f2da2db7b9b79e7ab7c3943d0b3381ab3
Edy Segura <ednardo@inatel.br>
2018-04-10 23:41:25 -0300

    Updated Map example

--- data-structure/map.js
+++ data-structure/map.js
@@ -6,11 +6,22 @@ jsMap.set('key2', 'value2')
 jsMap.set('key3', 'value3')
 jsMap.set('key4', 'value4')
 
+// map key can be an Object
 const objectKey = { email:'infoedy@gmail.com' }
 jsMap.set(objectKey, 'value5')
+
+// it also can be a function
+const functionKey = () => 'key5'
+jsMap.set(functionKey, 'value6')
+
+console.log('Map size: ', jsMap.size)
+console.log('-----------------------')
 
 for (const [ key, value ] of jsMap) {
   console.log(key, value)
 }
 
-console.log(jsMap.get(objectKey))
+console.log('Retrieving values')
+console.log('-----------------------')
+console.log('Object Key: ', jsMap.get(objectKey))
+console.log('Function Key: ', jsMap.get(functionKey))
