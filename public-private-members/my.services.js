c7497b14bb88e2f51bd2f9a1dd831db85630f438
Edy Segura <edysegura@gmail.com>
2019-07-22 23:26:16 -0300

    Update public/private example

--- public-private-members/my.services.js
+++ public-private-members/my.services.js
@@ -1,7 +1,18 @@
+'use strict'
+
 class MyService {
   #myPrivateMember = 'this is private'
   myPublicMember = "this is public"
+
+  // get myPrivateMember() {
+  //   return this.#myPrivateMember
+  // }
+
+  constructor() {
+    console.log('Accessing my private member: ' + this.#myPrivateMember)
+  }
 }
 
 const service = new MyService()
 console.log(service.myPublicMember)
+console.log(service.myPrivateMember)
